c7de84576397f72620f0c34211af7e3a8a4f2a2c
Abdul Moiz Khan <kabdulmoiz087@gmail.com>
2026-07-05 06:51:49 +0500

    Work on icons

--- app/Home/Components/ride.jsx
+++ app/Home/Components/ride.jsx
@@ -5,13 +5,13 @@ const rides = [
   {
     id: 1,
     title: "Economy Cars",
-    icon: "/icons/economy.png",
+    icon: "/icons/ecomny1.jpeg",
     image: "/images/alto.png",
     bg: "transparent",
 
-    width: "w-[270px] sm:w-[260px] lg:w-[400px]",
-    bottom: "-bottom-2 sm:-bottom-3 lg:-bottom-4",
-    right: "-right-53 sm:-right-16 lg:-right-88",
+    width: "w-[270px] sm:w-[260px] md:w-[270px] lg:w-[400px]",
+    bottom: "-bottom-2 sm:-bottom-3 md:-bottom-[-8px] lg:-bottom-4",
+    right: "-right-53 sm:-right-16 md:-right-54 lg:-right-88",
   },
   {
     id: 2,
@@ -20,9 +20,9 @@ const rides = [
     image: "/images/civic1.png",
     bg: "transparent",
 
-    width: "w-[240px] sm:w-[280px] lg:w-[430px]",
-    bottom: "-bottom-2 sm:-bottom-4 lg:-bottom-6",
-    right: "-right-48 sm:-right-16 lg:-right-90",
+    width: "w-[240px] sm:w-[280px] md:w-[240px] lg:w-[430px]",
+    bottom: "-bottom-2 sm:-bottom-4 md:-bottom-[-13px] lg:-bottom-6",
+    right: "-right-48 sm:-right-16 md:-right-50 lg:-right-90",
   },
   {
     id: 3,
@@ -31,9 +31,9 @@ const rides = [
     image: "/images/tucso1.png",
     bg: "transparent",
 
-    width: "w-[230px] sm:w-[270px] lg:w-[390px]",
-    bottom: "-bottom-6 sm:-bottom-8 lg:-bottom-20",
-    right: "-right-48 sm:-right-14 lg:-right-82",
+    width: "w-[230px] sm:w-[270px] md:w-[250px] lg:w-[390px]",
+    bottom: "-bottom-6 sm:-bottom-8 md:-bottom-10 lg:-bottom-20",
+    right: "-right-48 sm:-right-14 md:-right-50 lg:-right-82",
   },
   {
     id: 4,
@@ -42,9 +42,9 @@ const rides = [
     image: "/images/camry.png",
     bg: "transparent",
 
-    width: "w-[350px] sm:w-[340px] lg:w-[600px]",
-    bottom: "-bottom-6 sm:-bottom-8 lg:-bottom-20",
-    right: "-right-63 sm:-right-20 lg:-right-115",
+    width: "w-[350px] sm:w-[340px] md:w-[350px] lg:w-[600px]",
+    bottom: "-bottom-6 sm:-bottom-8 md:-bottom-[-20px] lg:-bottom-20",
+    right: "-right-63 sm:-right-20 md:-right-65 lg:-right-115",
   },
 ];
 export default function ChooseRide() {
@@ -74,7 +74,8 @@ export default function ChooseRide() {
               style={{ backgroundColor: ride.bg }}
             >
               {/* Left Content */}
-              <div className="relative w-[55%] sm:w-1/2 p-5 sm:p-6 md:p-8 text-white z-10">
+              {/* <div className="relative w-[55%] sm:w-1/2 p-5 sm:p-6 md:p-8 text-white z-10"> */}
+              <div className="relative w-[58%] sm:w-[55%]  md:w-[50%] lg:w-1/2 p-5 sm:p-6 md:p-7 lg:p-8 text-white z-10">
                 {/* <div className="w-12 h-12 rounded-full bg-[#fd0014] flex items-center justify-center mb-4 shadow-md">
                   <Image
                     src={ride.icon}
@@ -85,18 +86,33 @@ export default function ChooseRide() {
                   />
                 </div> */}
                 <div
+                  //  className={`absolute ${ride.right} ${ride.bottom} md:-right-[200px]  z-20 pointer-events-none`}
                   className={`absolute ${ride.right} ${ride.bottom} z-20 pointer-events-none`}
                 >
                   <Image
+                  
                     src={ride.image}
                     alt={ride.title}
                     width={600}
                     height={400}
                     className={`${ride.width} h-auto object-contain`}
+                    // className={`${ride.width} md:w-[260px] lg:${ride.width} h-auto object-contain`}
                   />
                 </div>
 
-                <h3 className="text-xl sm:text-2xl font-bold mb-5 sm:mb-6 leading-snug text-black">
+                <h3
+                  // className="text-xl sm:text-2xl md:text-[15px] lg:text-2xl font-bold mb-5 sm:mb-6 md:mb-4 leading-snug md:leading-6 text-black">
+                  className="text-xl sm:text-2xl md:text-[15px] lg:text-2xl font-bold mb-5 sm:mb-6 md:mb-4 leading-snug md:leading-6 text-black md:-ml-4"
+                >
+                  {/* {ride.icon && (
+                    <Image
+                      src={ride.icon}
+                      alt={ride.title}
+                      width={50}
+                      height={50}
+                      className="object-contain mr-2"
+                    />
+                  )} */}
                   {ride.title}
                 </h3>
 
@@ -109,7 +125,9 @@ export default function ChooseRide() {
                 </Link> */}
                 <Link
                   href={`/vehicles?category=${encodeURIComponent(ride.title)}`}
-                  className="inline-flex items-center gap-2 bg-white hover:bg-red-50 border-2 border-red-600 text-red-600 px-4 py-2 rounded-md text-sm font-medium transition"
+                    className="inline-flex items-center bg-white hover:bg-red-50 border-2 border-red-600 text-red-600 px-4 py-2 md:px-2 md:py-1.5 md:text-[12px] lg:px-4 lg:py-2 lg:text-sm rounded-md font-medium transition md:-ml-4"
+
+                  // className="inline-flex items-center  bg-white hover:bg-red-50 border-2 border-red-600 text-red-600 px-4 py-2 md:px-2 md:py-1.5 md:text-[12px]  lg:px-4 lg:py-2 lg:text-sm rounded-md font-medium transition"
                 >
                   View Vehicles
                   <span>→</span>
